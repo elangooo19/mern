ee67e8b0e620f8322b46f68f782341b3b3c7e219
elangooo19 <elangosumathi8@gmail.com>
2025-12-08 03:48:15 +0530

    update

---
+++ scripts.js
@@ -0,0 +1,37 @@
+var a; //global scope
+a=10;
+var a = 20;
+let b; //block scope
+b=10;
+b=20;
+const c=10;
+
+
+var p = 20; //number
+var q = "Hello";//string
+var bool = true;//bolllean
+var r; //undefine
+var s = null;//null
+
+var arr=[1,2,3,4,5];
+console.log(arr);
+
+
+var object = {  //object
+    name : "Raj",
+    age : 20
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
