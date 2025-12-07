15ce596342d9d067a127639eccdab296fee9b9b9
elangooo19 <elangosumathi8@gmail.com>
2025-12-08 04:49:53 +0530

    update

--- scripts.js
+++ scripts.js
@@ -22,16 +22,76 @@ var object = {  //object
     age : 20
 }
 
+//Arithmetic Operator
+var x = 10;
+var y = 20;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+//relational operator
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x==y);
+console.log(x!=y);
 
 
 
+//Assign operator
+var z = x+y;
+console.log(z);
+z+=2;
+console.log(z);
+z-=2;
+console.log(z);
+z*=2;
+console.log(z);
+z/=2;
+console.log(z);
+z%=2;
 
 
+//logical operator
+a=true;
+b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
+console.log(!b);
+
+//for loop
+for(var i=0;i<10;i++){
+    console.log(i);
+}
+//while loop
+var (j=0;){
+    //statement
+    j++;
+}
+var a = 10;
+while(a>0){
+    console.log(a);
+    a--;
+}
+//do while loop
+var b = 10; 
+do{
+    console.log(b);
+    b--;
+}while(b>0);
 
 
-
-
-
-
+//condition statement
+if(a>0){
+    console.log("a is positive");
+}
+else if(a<0){
+    console.log("a is negative");
+} else{
+    console.log("a is zero");
+}
 
 
