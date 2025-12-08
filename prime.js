fc5547291f089e59c38ce121f8f4fe114a51c81b
elangooo19 <elangosumathi8@gmail.com>
2025-12-09 03:47:06 +0530

    update

--- prime.js
+++ prime.js
@@ -60,79 +60,106 @@
 
 
 //callback function
-function add(a,b){
-    console.log(a+b);
-}
-function sub(a,b){
-    console.log(a-b);
-}
-function mul(a,b){
-    console.log(a*b);
-}
-function div(a,b){
-    console.log(a/b);
-}
-function calc(a,b,op){
-    op(a,b);
-}
-calc(10,20,add);
-calc(10,20,sub);
-calc(10,20,mul);
-calc(10,20,div);
+// function add(a,b){
+//     console.log(a+b);
+// }
+// function sub(a,b){
+//     console.log(a-b);
+// }
+// function mul(a,b){
+//     console.log(a*b);
+// }
+// function div(a,b){
+//     console.log(a/b);
+// }
+// function calc(a,b,op){
+//     op(a,b);
+// }
+// calc(10,20,add);
+// calc(10,20,sub);
+// calc(10,20,mul);
+// calc(10,20,div);
 
-//arrow function
-var add = (a,b) => {
-    console.log(a+b);
-}
-var sub = (a,b) => {
-    console.log(a-b);
-}
-var mul = (a,b) => {
-    console.log(a*b);
-}
-var div = (a,b) => {
-    console.log(a/b);
-}
-var calc = (a,b,op) => {
-    op(a,b);
-}
-calc(10,20,add);
-calc(10,20,sub);
-calc(10,20,mul);
-calc(10,20,div);
+// //arrow function
+// var add = (a,b) => {
+//     console.log(a+b);
+// }
+// var sub = (a,b) => {
+//     console.log(a-b);
+// }
+// var mul = (a,b) => {
+//     console.log(a*b);
+// }
+// var div = (a,b) => {
+//     console.log(a/b);
+// }
+// var calc = (a,b,op) => {
+//     op(a,b);
+// }
+// calc(10,20,add);
+// calc(10,20,sub);
+// calc(10,20,mul);
+// calc(10,20,div);
 
-//array
-var arr = [1,2,3,4,5];
-for(var i in arr){
-    console.log(arr[i]);
-}
-//for...of(values)
-for(var i of arr){
-    console.log(i);
-}
+// //array
+// var arr = [1,2,3,4,5];
+// for(var i in arr){
+//     console.log(arr[i]);
+// }
+// //for...of(values)
+// for(var i of arr){
+//     console.log(i);
+// }
 
-arr.forEach(function(i){
-    console.log(i);
-});
+// arr.forEach(function(i){
+//     console.log(i);
+// });
 
-//class
-class person{
-    constructor(name,age){
-        this.name = name;
-        this.age = age;
-    }
-    print(){
-        console.log(this.name,this.age);
-    }
-}
-var p1 = new person("Raj", 20);
-p1.print();
+// //class
+// class person{
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     print(){
+//         console.log(this.name,this.age);
+//     }
+// }
+// var p1 = new person("Raj", 20);
+// p1.print();
 
 
-var add = (a, b,callback) => {
-    var result = a+b;
-    console.log(result);
-}
-add(10, 20, (result) => {
-    console.log(result);
-});
+// var add = (a, b,callback) => {
+//     var result = a+b;
+//     console.log(result);
+// }
+// add(10, 20, (result) => {
+//     console.log(result);
+// });
+
+
+
+//Template literals
+// var name = "Raj"
+// var str = 'Raj his age is $(age)'
+// var age = 25;
+// console.log(str);
+// console.log('${name} age is ${age}')
+
+// //map
+// var arr = [1,2,3,4,5];
+// var arr1 = arr.map((i) => {
+//     return i*2;
+//     console.log(i);
+// });
+
+// filter 
+// var arr = arr.filter((i) => {
+//     return i%2==0;
+//     console.log(i); 
+// });
+
+// //Reduce
+// var total = arr. reduce((sum, num)=>(sum+num),0)
+// console.log(total);
+
