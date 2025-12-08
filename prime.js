b02c5b644d45fa520998138330b7c79340d468dd
elangooo19 <elangosumathi8@gmail.com>
2025-12-08 08:39:00 +0530

    update

--- prime.js
+++ prime.js
@@ -13,3 +13,27 @@ if(prime==0)
 {
 console.log("Prime");
 }
+
+var a=424;
+
+while(a!=0;)
+    var rem=a%10;
+    sum=sum*10+rem;
+    a=parseInt(a/10);
+    console.log(sum);
+    if(sum==a)
+    {
+        console.log("Palindrome");
+    }
+    else
+    {
+        console.log("Not Palindrome");
+    }
+    var a = 10;
+
+    if(true){
+        var a = 20;
+        console.log("a is positive");
+    }// gobal scope
+    console.log(a);
+    
